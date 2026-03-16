4535792e34d5414e607ec3dfa1e2d7fd4b3aa9e7
jul2264 <julianstevenj@gmail.com>
2026-03-16 08:43:53 +0530

    Deque-Based Optimized Palindrome Checker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,30 +1,27 @@
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
 import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Palindrome Checker Management System\n" +
-                "Version : 1.0\n" +
-                "System initialized successfully.");
-        System.out.print("Input text: ");
-        String input = sc.next();
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
-        boolean isPalindrome = true;
-        for (char ch : input.toCharArray()) {
-            stack.push(ch);
-            queue.add(ch);
+        String input = "level";
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
         }
-        while (!stack.isEmpty()) {
-            if (!stack.pop().equals(queue.remove())) {
+
+        boolean isPalindrome = true;
+        while (list.size() > 1) {
+
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
         }
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
-        sc.close();
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
